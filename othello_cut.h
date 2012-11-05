1fcc0a33d569480e867dcf36cf00dbdc97a67cf8
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2012-11-05 16:24:21 -0430

    ...

--- othello_cut.h
+++ othello_cut.h
@@ -79,6 +79,7 @@ static int PV[] = {
 };
 
 class state_t {
+  public:
     unsigned char t_; 
     unsigned free_;
     unsigned pos_;
@@ -287,15 +288,15 @@ inline state_t state_t::move(bool color, int pos) const {
     while( *x != pos ) ++x;
     if( *(x+1) != -1 ) {
         for( p = x + 1; (*p != -1) && !is_free(*p) && (color ^ is_black(*p)); ++p );
-        if( (p > x + 1 ) && (*p != -1) && !is_free(*p) ) {
-            for( const int *q = x + 1; q < p; ++q) s.set_color(color,*q);
+        if( (p > x + 1) && (*p != -1) && !is_free(*p) ) {
+            for( const int *q = x + 1; q < p; ++q ) s.set_color(color, *q);
         }
     }
-	if( x != dia1[pos - 4] ) {
-        for( p = x - 1; (p >= dia1[pos - 4]) && !is_free(*p) && (color ^ is_black(*p)); --p);
-        if( (p < x - 1) && (p >= dia1[pos - 4]) && !is_free(*p) ){
-            for( const int *q = x - 1; q > p; --q ) s.set_color(color,*q);
-         }
+    if( x != dia1[pos - 4] ) {
+        for( p = x - 1; (p >= dia1[pos - 4]) && !is_free(*p) && (color ^ is_black(*p)); --p );
+        if( (p < x - 1) && (p >= dia1[pos - 4]) && !is_free(*p) ) {
+            for( const int *q = x - 1; q > p; --q ) s.set_color(color, *q);
+        }
     }
 
     // Process diagonal 2
@@ -303,16 +304,16 @@ inline state_t state_t::move(bool color, int pos) const {
     while( *x != pos ) ++x;
     if( *(x+1) != -1 ) {
         for( p = x + 1; (*p != -1) && !is_free(*p) && (color ^ is_black(*p)); ++p );
-        if( (p > x + 1 ) && (*p != -1) && !is_free(*p) ) {
-            for( const int *q = x + 1; q < p; ++q) s.set_color(color,*q);
+        if( (p > x + 1) && (*p != -1) && !is_free(*p) ) {
+            for( const int *q = x + 1; q < p; ++q ) s.set_color(color, *q);
         }
     }
-	if( x != dia2[pos - 4] ) {
-        for( p = x - 1; (p >= dia2[pos - 4]) && !is_free(*p) && (color ^ is_black(*p)); --p);
-        if( (p < x - 1) && (p >= dia2[pos - 4]) && !is_free(*p) ){
-            for( const int *q = x - 1; q > p; --q ) s.set_color(color,*q);
-         }
-    }	
+    if( x != dia2[pos - 4] ) {
+        for( p = x - 1; (p >= dia2[pos - 4]) && !is_free(*p) && (color ^ is_black(*p)); --p );
+        if( (p < x - 1) && (p >= dia2[pos - 4]) && !is_free(*p) ) {
+            for( const int *q = x - 1; q > p; --q ) s.set_color(color, *q);
+        }
+    }
 	
     return s;
 }
