f01bfed7b898602028fedb28c59a9e39c6f811d9
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2012-11-05 21:37:51 -0430

    negascout al parecer listo

--- negascout.cc
+++ negascout.cc
@@ -5,10 +5,11 @@ using namespace std;
 
 
 int negascout(state_t s, int alpha, int beta, bool color) {
+  cout << s << endl;
   if (s.terminal())
      return s.value();
   int b = beta;
-  vector<int> succ_ = s.succ(color);
+  vector<int> succ_ = s.succ(!color);
   for(vector<int>::const_iterator iter=succ_.begin(),limit=succ_.end();iter!=limit;++iter) {
     state_t n = s.move(!color,*iter);
     int score = -negascout(n,-b,-alpha,!color);
@@ -23,9 +24,77 @@ int negascout(state_t s, int alpha, int beta, bool color) {
 }
 
 int main() {
-  state_t root = state_t();
+ state_t root = state_t();
+ root=root.move(true,12);
+ cout << root << endl;
+ root=root.move(!true,21);
+ cout << root << endl;;
+ root=root.move(true,26);
+ cout << root << endl;;
+ root=root.move(!true,13);
+ cout << root << endl;
+ root=root.move(true,22);
+ cout << root << endl;
+ root=root.move(!true,18);
+ cout << root << endl;
+ root=root.move(true,7);
+ cout << root << endl;
+ root=root.move(!true,6);
+ cout << root << endl;
+ root=root.move(true,5);
+ cout << root << endl;
+ root=root.move(!true,27);
+ cout << root << endl;
+ root=root.move(true,33);
+ cout << root << endl;
+ root=root.move(!true,23);
+ cout << root << endl;
+ root=root.move(true,17);
+ cout << root << endl;
+ root=root.move(!true,11);
+ cout << root << endl;
+ root=root.move(true,19);
+ cout << root << endl;
+ root=root.move(!true,15);
+ cout << root << endl;
+ root=root.move(true,14);
+ cout << root << endl;
+ root=root.move(!true,31);
+ cout << root << endl;
+ root=root.move(true,20);
+ cout << root << endl;
+ root=root.move(!true,32);
+ cout << root << endl;
+ root=root.move(true,30);
+ cout << root << endl;
+ root=root.move(!true,10);
+ cout << root << endl;
+ root=root.move(true,25);
+ cout << root << endl;
+ root=root.move(!true,24);
+ cout << root << endl;
+ root=root.move(true,34);
+ cout << root << endl;
+ root=root.move(!true,28);
+ cout << root << endl;
+ root=root.move(true,16);
+ cout << root << endl;
+ root=root.move(!true,4);
+ cout << root << endl;
+ root=root.move(true,29);
+ cout << root << endl;
+ root=root.move(!true,35);
+ cout << root << endl;
+ root=root.move(true,36);
+ cout << root << endl;
+ root=root.move(!true,8);
+ cout << root << endl;
+ /*root=root.move(true,9);
+ cout << root << endl;*/
+ cout << "Valor:";
+ cout << root.value() << endl;
   
-  int v = negascout(root,numeric_limits<int>::min(),numeric_limits<int>::max(),true);
+  int v = negascout(root,numeric_limits<signed int>::min(),numeric_limits<signed int>::max(),false);
   
   cout << v << endl;
 }
