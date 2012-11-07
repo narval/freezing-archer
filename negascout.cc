8a85cf6db5b38706c4685f26f337d015c7ea3933
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2012-11-06 22:28:46 -0430

    negascout version 100

--- negascout.cc
+++ negascout.cc
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-int negascout(state_t s, int alpha, int beta, bool color) {
+
+/*int negascout(state_t s, int alpha, int beta, bool color) {
   if (s.terminal())
      return s.value() * (color ? 1 : -1);
   int b = beta;
@@ -18,20 +19,157 @@ int negascout(state_t s, int alpha, int beta, bool color) {
     succ_ = s.succ(color_);
   }
   
-  cout << succ_.size() << " - - " << color_ << " === " << succ_.front() << endl;
-  
   for(vector<int>::const_iterator iter=succ_.begin(),limit=succ_.end();iter!=limit;++iter) {
     state_t n = s.move(color_,*iter);
     int score = -negascout(n,-b,-alpha,!color_);
 	if ((alpha < score) && (score < beta) && (*iter!=*(succ_.begin())))
 	  score = -negascout(n,-beta,-alpha,!color_);
-    alpha = std::max(alpha,score);
+    alpha = max(alpha,score);
     if (alpha >= beta)
       return alpha;
     b = alpha + 1;	  
   }
   return alpha;
+}*/
+
+int G_(state_t s, int alpha, int beta, bool color);
+
+int F_(state_t s, int alpha, int beta, bool color) {
+  if (s.terminal())
+    return s.value() * (color ? 1 : -1);
+	
+  int m = numeric_limits<signed int>::min();
+  
+  bool color_ = color;
+  vector<int> succ_ = s.succ(color_);
+  int _succ = succ_.size();
+  
+  state_t n;
+  
+  if (_succ) { 
+    n = s.move(color_,succ_.front());
+    m = max(m,G_(n,alpha,beta,!color_));
+    if(m >= beta)
+      return m;
+  
+    for(vector<int>::const_iterator iter=succ_.begin(),limit=succ_.end();iter!=limit;++iter) {
+      if (iter != succ_.begin()) {
+	    n = s.move(color_,*iter);
+	    int t = G_(n,m,m + 1,!color_);
+	    if (t > m)
+          if (t >= beta) 
+             m = t;
+          else
+             m = G_(n,t,beta,!color_);
+        if (m >= beta)
+          return m;
+	  }
+    }
+  }
+  else {
+    m = max(m,G_(s,alpha,beta,!color_));
+	if(m >= beta)
+      return m;
+	  
+	int t = G_(s,m,m + 1,!color_);
+	if (t > m)
+      if (t >= beta) 
+         m = t;
+      else
+         m = G_(s,t,beta,!color_);
+    if (m >= beta)
+      return m;
+  }  
+  
+  return m;
 }
+
+int G_(state_t s, int alpha, int beta, bool color) {
+  if (s.terminal())
+    return s.value() * (color ? 1 : -1);
+	
+  int m = numeric_limits<signed int>::max();
+   
+  bool color_ = color;
+  vector<int> succ_ = s.succ(color_);
+  int _succ = succ_.size();
+  
+  state_t n;
+  
+  if (_succ) {
+    n = s.move(color_,succ_.front());
+    m = min(m,F_(n,alpha,beta,!color_));
+    if(m <= alpha)
+      return m;
+
+    for(vector<int>::const_iterator iter=succ_.begin(),limit=succ_.end();iter!=limit;++iter) {
+      if (iter!=succ_.begin()) {
+	    n = s.move(color_,*iter);
+	    int t = F_(n,m,m + 1,!color_);
+	    if (t <= m)
+          if (t <= alpha) 
+            m = t;
+          else
+           m = F_(n,alpha,t,!color_);
+        if (m <= alpha)
+        return m;	  
+	  }
+    }
+  }
+  else {
+    m = min(m,F_(s,alpha,beta,!color_));
+    if(m <= alpha)
+      return m;
+	  
+	int t = F_(s,m,m + 1,!color_);
+	if (t <= m)
+      if (t <= alpha) 
+         m = t;
+      else
+         m = F_(s,alpha,t,!color_);
+    if (m <= alpha)
+      return m;	  
+  }
+  
+  return m;
+}
+
+int negascout(state_t s, int alpha, int beta, bool color) {
+  if (color)
+    return F_(s,alpha,beta,color);
+  else
+    return G_(s,alpha,beta,color);
+}
+
+/*int Nega_Scout(state_t s, int alpha, int beta, bool color) {
+  if (s.terminal())
+    return s.value() * (color ? 1 : -1);
+
+  bool color_ = color;
+  vector<int> succ_ = s.succ(color_);
+  int _succ = succ_.size();
+ 
+  
+  // Pasar el turno al jugador contrario
+  if(!_succ) {
+    color_ = !color; 
+    succ_ = s.succ(color_);
+  }
+  
+  int a = alpha, b = beta;
+  
+  for(vector<int>::const_iterator iter=succ_.begin(),limit=succ_.end();iter!=limit;++iter) {
+    state_t n = s.move(color_,*iter);
+    int t = -Nega_Scout(n,-b,-a,!color_);
+	if((t > a) && (t < beta) && ((*iter) != succ_.front()))
+	  a = Nega_Scout(n,-beta,-t,!color_);
+	a = max(a,t);
+	if(a >= beta)
+	  return a;
+	b = a + 1;
+  }
+  return a;
+}*/
 
 int main() {
  state_t root = state_t();
@@ -83,15 +221,15 @@ int main() {
  cout << root << endl;
  root=root.move(!true,24);
  cout << root << endl;
- root=root.move(true,34);
+ /*root=root.move(true,34);
  cout << root << endl;
  root=root.move(!true,28);
  cout << root << endl;
- root=root.move(true,16);// 8
+ root=root.move(true,16);
  cout << root << endl;
- root=root.move(!true,4); //-4
+ root=root.move(!true,4);
  cout << root << endl;
- /*root=root.move(true,29); // 8
+ root=root.move(true,29);
  cout << root << endl;
  root=root.move(!true,35);
  cout << root << endl;
@@ -101,9 +239,9 @@ int main() {
  cout << root << endl;
  root=root.move(true,9);
  cout << root << endl;*/
- cout << "Valor:" << root.value() << endl;
+ cout << "Valor: " << root.value() << endl;
   
- int v = negascout(root,numeric_limits<signed int>::min(),numeric_limits<signed int>::max(),!true);
+ int v = negascout(root,numeric_limits<signed int>::min(),numeric_limits<signed int>::max(),true);
  
 //std::vector<int> v();  
  cout << v << endl;
